a998fbb4ba82ebd7f405c082cb5a08f0b610a2ce
Demonware023 <awamchimere@icloud.com>
2024-09-22 10:09:08 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,14 +1,14 @@
 -- List Glam rock bands and their lifespan until 2022
-
 SELECT 
-    band_name,
+    DISTINCT band_name,
     CASE
-        WHEN split IS NOT NULL THEN YEAR(split) - YEAR(formed)
+        WHEN split IS NOT NULL AND YEAR(split) >= YEAR(formed) THEN YEAR(split) - YEAR(formed)
         ELSE 2022 - YEAR(formed)
     END AS lifespan
 FROM 
     metal_bands
 WHERE 
     main_style = 'Glam rock'
+    AND formed IS NOT NULL
 ORDER BY 
     lifespan DESC;
